fccc799a00f3d1226ce971a7eb5361f00748a201
tulqhe160053 <122227097+tulqhe160053@users.noreply.github.com>
2023-01-15 16:06:21 +0700

    Add files via upload

---
+++ ISP392/Database/createTable.sql
@@ -0,0 +1,178 @@
+﻿
+use OnlineShop
+
+---------------------------------------------- Table user status -------------------------------
+
+CREATE TABLE UserStatus (
+	ID int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+	StatusName nvarchar(1000)
+)
+GO
+
+-- select * from UserStatus
+
+-------------------------------------------------- Table role -------------------------------
+
+Create table [Role](
+	RoleID int IDENTITY(1, 1) primary key,
+	RoleName nvarchar (20),
+	-- Admin,marketing, seller, customer
+);
+
+GO
+
+--select * from Role
+
+---------------------------- table Users ---------------------------------
+create table Users
+(
+	UserID int PRIMARY KEY identity(1,1),
+	Username nvarchar(50),
+	[Password] varchar(50),
+	Email nvarchar(100),
+	[RoleID] int default 3,
+	statusId int,
+	FOREIGN KEY(RoleID) REFERENCES [Role](RoleID),
+	FOREIGN KEY (statusId)
+	REFERENCES UserStatus (id)
+)
+
+--select * from Users
+------------------------------------------------ Table Ship --------------------------------
+CREATE TABLE Ship (
+	id int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+	CityName nvarchar(1000) ,
+	ShipPrice int
+)
+
+--select * from Ship
+
+----------------------------- Table UserAddress -------------------
+
+CREATE TABLE UserAddress (
+	ID int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+	UserID int,
+	ShipName nvarchar(500),
+	ShipAddress nvarchar(1000),
+	ShipCityID int,
+	PhoneNum varchar(20),
+	constraint userID_in_user_address FOREIGN KEY(UserID) REFERENCES Users(UserID),
+	constraint ship_city_in_ship_address FOREIGN KEY(ShipCityID) REFERENCES Ship(id)
+)
+
+--select * from UserAddress
+
+----------------------------- Table Category -------------------
+
+CREATE TABLE Category (
+	CategoryID int PRIMARY KEY identity(1,1),
+	CategoryName nvarchar(1000),
+)
+
+--select * from Category
+
+----------------------------- Table subcategory ----------------------------
+
+CREATE TABLE [dbo].[SubCategory](
+	[SubCategoryID] [int] PRIMARY KEY  IDENTITY(1,1) ,
+	[SubCategoryName] [nvarchar](255),
+	[CategoryID] [int] ,
+	constraint CategoryID FOREIGN KEY (CategoryID)  REFERENCES Category(CategoryID)
+)
+
+--select * from [SubCategory]
+
+------------------------------- Table ProductStatus -------------------------
+
+CREATE TABLE ProductStatus (
+	StatusID int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+	StatusName nvarchar(1000)
+)
+
+--select * from ProductStatus
+
+------------------------------- Table Brand -------------------------
+
+CREATE TABLE Brand (
+	BrandID int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+	BrandName nvarchar(1000)
+) 
+
+--select * from Brand
+
+------------------------------- Table Product -------------------------
+
+CREATE TABLE Product (
+	ProductID int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+	ProductName nvarchar(1000),
+	[Description] text,
+	OriginalPrice int, 
+	SellPrice int,	
+	SalePercent int CHECK (SalePercent>=0 AND SalePercent<=100),
+	SubCategoryID int,
+	SellerID int,
+	Amount int,
+	StatusID int,
+	BrandID int,
+	constraint product_in_subCategory FOREIGN KEY(SubCategoryID) REFERENCES SubCategory(SubCategoryID),
+	constraint SellerID_in_Users FOREIGN KEY(SellerID) REFERENCES Users(UserID),
+	constraint StatusID_in_Status FOREIGN KEY(StatusID) REFERENCES ProductStatus(StatusID),
+	constraint BrandID_in_Brand FOREIGN KEY(BrandID) REFERENCES Brand(BrandID)
+)
+
+--select * from Product
+
+--------------------- Table ProductImg -------------------
+
+CREATE TABLE [dbo].[ProductImg](
+	[ProductID] [int]  ,
+	[ProductImgURL] [varchar](255) ,
+	constraint ProductImg_ID FOREIGN KEY([ProductID])
+	REFERENCES [dbo].[Product] ([ProductID])
+)
+
+
+--------------------- Table cart ---------------------
+
+CREATE TABLE Cart (
+	UserID int,
+	ProductID int,
+	Amount int
+	constraint userID_in_cart FOREIGN KEY(UserID) REFERENCES Users(UserID),
+	constraint productID_in_cart FOREIGN KEY(ProductID) REFERENCES Product(ProductID),
+)
+
+--select * from Cart
+
+--------------------- Table OrderStatus -----------------
+
+CREATE TABLE [dbo].[OrderStatus](
+	[ID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[Name] [nvarchar](50) NOT NULL,
+ )
+
+ -------------------- Table Orders ----------------------
+
+ CREATE TABLE [dbo].[Orders](
+	[ID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[UserID] [int] NULL,
+	[TotalPrice] [int] NULL,
+	[Note] [nvarchar](2000) NULL,
+	[Status] [int] NULL,
+	[Date] [DATETIME] NULL ,
+	constraint userID_in_order_status FOREIGN KEY(UserID) REFERENCES Users(UserID),
+	constraint statusID_in_order_status FOREIGN KEY(Status) REFERENCES OrderStatus(ID)
+)
+
+--------------------- Table OrderDetail ---------------------
+
+CREATE TABLE [dbo].[OrderDetail](
+	ID int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+	[OrderID] [int] NOT NULL ,
+	[ProductID] [int] NOT NULL,
+	[ProductName] [nvarchar](1000) NOT NULL,
+	[ProductPrice] [int] NOT NULL,	
+	[Quantity] [int] NOT NULL,
+	constraint orderID_in_order_detail FOREIGN KEY(OrderID) REFERENCES Orders(ID),
+	constraint productID_in_order_detail FOREIGN KEY(ProductID) REFERENCES Product(ProductID)	
+)
---
+++ ISP392/Database/insertData.sql
@@ -0,0 +1,192 @@
+﻿
+use OnlineShop
+
+------ Insert into UserStatus ------------------------
+INSERT INTO UserStatus VALUES (N'Active');
+INSERT INTO UserStatus VALUES (N'Locked');
+INSERT INTO UserStatus VALUES (N'Gmail Login');
+INSERT INTO UserStatus VALUES (N'Unverified');
+
+------ Insert into role ------------
+
+INSERT INTO [Role] VALUES (N'Admin');
+INSERT INTO Role VALUES (N'Seller');
+INSERT INTO Role VALUES (N'Customer');
+INSERT INTO Role VALUES (N'Marketing');
+INSERT INTO Role VALUES (N'Shipper');
+
+------ Insert into Users ------------
+
+INSERT INTO Users VALUES(N'admin','123456','admin@fpt.edu.vn',1,1);
+INSERT INTO Users VALUES(N'tu','1234567','tulqhe160053@fpt.edu.vn',3,1);
+INSERT INTO Users VALUES(N'thanh','654321','thanh01@gmail.com',2,1);
+INSERT INTO Users VALUES(N'mkt','mkt123','mkt@gmail.com',4,1);
+INSERT INTO Users VALUES(N'shipper','shipper','shipper@fpt.edu.vn',5,1);
+
+------ Insert into Ship ------------
+
+INSERT INTO Ship VALUES (N'An Giang', 30000);
+INSERT INTO Ship VALUES (N'Bà Rịa-Vũng Tàu', 30000);
+INSERT INTO Ship VALUES (N'Bạc Liêu', 30000);
+INSERT INTO Ship VALUES (N'Bắc Kạn', 30000);
+INSERT INTO Ship VALUES (N'Bắc Giang', 30000);
+INSERT INTO Ship VALUES (N'Bắc Ninh', 30000);
+INSERT INTO Ship VALUES (N'Bến Tre', 30000);
+INSERT INTO Ship VALUES (N'Bình Dương', 30000);
+INSERT INTO Ship VALUES (N'Bình Định', 30000);
+INSERT INTO Ship VALUES (N'Bình Phước', 30000);
+INSERT INTO Ship VALUES (N'Bình Thuận', 30000);
+INSERT INTO Ship VALUES (N'Cà Mau', 30000);
+INSERT INTO Ship VALUES (N'Cao Bằng', 30000);
+INSERT INTO Ship VALUES (N'Cần Thơ', 30000);
+INSERT INTO Ship VALUES (N'Đà Nẵng', 30000);
+INSERT INTO Ship VALUES (N'Đắk Lắk', 30000);
+INSERT INTO Ship VALUES (N'Đắk Nông', 30000);
+INSERT INTO Ship VALUES (N'Điện Biên', 30000);
+INSERT INTO Ship VALUES (N'Đồng Nai', 30000);
+INSERT INTO Ship VALUES (N'Đồng Tháp', 30000);
+INSERT INTO Ship VALUES (N'Gia Lai', 30000);
+INSERT INTO Ship VALUES (N'Hà Giang', 30000);
+INSERT INTO Ship VALUES (N'Hà Nam', 30000);
+INSERT INTO Ship VALUES (N'Hà Nội', 20000);
+INSERT INTO Ship VALUES (N'Hà Tĩnh', 30000);
+INSERT INTO Ship VALUES (N'Hải Dương', 30000);
+INSERT INTO Ship VALUES (N'Hải Phòng', 30000);
+INSERT INTO Ship VALUES (N'Hòa Bình', 30000);
+INSERT INTO Ship VALUES (N'Hồ Chí Minh', 30000);
+INSERT INTO Ship VALUES (N'Hậu Giang', 30000);
+INSERT INTO Ship VALUES (N'Hưng Yên', 30000);
+INSERT INTO Ship VALUES (N'Khánh Hòa', 30000);
+INSERT INTO Ship VALUES (N'Kiên Giang', 30000);
+INSERT INTO Ship VALUES (N'Kon Tum', 30000);
+INSERT INTO Ship VALUES (N'Lai Châu', 30000);
+INSERT INTO Ship VALUES (N'Lào Cai', 30000);
+INSERT INTO Ship VALUES (N'Lạng Sơn', 30000);
+INSERT INTO Ship VALUES (N'Lâm Đồng', 30000);
+INSERT INTO Ship VALUES (N'Long An', 30000);
+INSERT INTO Ship VALUES (N'Nam Định', 30000);
+INSERT INTO Ship VALUES (N'Nghệ An', 30000);
+INSERT INTO Ship VALUES (N'Ninh Bình', 30000);
+INSERT INTO Ship VALUES (N'Ninh Thuận', 30000);
+INSERT INTO Ship VALUES (N'Phú Thọ', 30000);
+INSERT INTO Ship VALUES (N'Phú Yên', 30000);
+INSERT INTO Ship VALUES (N'Quảng Bình', 30000);
+INSERT INTO Ship VALUES (N'Quảng Nam', 30000);
+INSERT INTO Ship VALUES (N'Quảng Ngãi', 30000);
+INSERT INTO Ship VALUES (N'Quảng Ninh', 30000);
+INSERT INTO Ship VALUES (N'Quảng Trị', 30000);
+INSERT INTO Ship VALUES (N'Sóc Trăng', 30000);
+INSERT INTO Ship VALUES (N'Sơn La', 30000);
+INSERT INTO Ship VALUES (N'Tây Ninh', 30000);
+INSERT INTO Ship VALUES (N'Thái Bình', 30000);
+INSERT INTO Ship VALUES (N'Thái Nguyên', 30000);
+INSERT INTO Ship VALUES (N'Thanh Hóa', 30000);
+INSERT INTO Ship VALUES (N'Thừa Thiên - Huế', 30000);
+INSERT INTO Ship VALUES (N'Tiền Giang', 30000);
+INSERT INTO Ship VALUES (N'Trà Vinh', 30000);
+INSERT INTO Ship VALUES (N'Tuyên Quang', 30000);
+INSERT INTO Ship VALUES (N'Vĩnh Long', 30000);
+INSERT INTO Ship VALUES (N'Vĩnh Phúc', 30000);
+INSERT INTO Ship VALUES (N'Yên Bái', 30000);
+
+------ Insert into UserAddress ------------
+
+insert into dbo.[UserAddress] ([UserID], [ShipName], [ShipAddress], [ShipCityID]) values (1, 'admin', N'119 Đường Láng', 24);
+insert into dbo.[UserAddress] ([UserID], [ShipName], [ShipAddress], [ShipCityID],[PhoneNum]) values (2, 'Giao hang tiet kiem', N'Đại học Fpt', 24, '09154291');
+insert into dbo.[UserAddress] ([UserID], [ShipName], [ShipAddress], [ShipCityID],[PhoneNum]) values (3, 'Giao hang nhanh', N'180 Cầu Giấy', 24, '0123123');
+insert into dbo.[UserAddress] ([UserID], [ShipName], [ShipAddress], [ShipCityID]) values (4, 'Grabship', N'92 Đống Đa', 10);
+insert into dbo.[UserAddress] ([UserID], [ShipName], [ShipAddress], [ShipCityID],[PhoneNum]) values (5, 'VNExpress', N'119 Lê Thánh Tông', 24,'0123456789');
+
+------ Insert into Category ------------
+
+INSERT INTO dbo.Category(CategoryName)VALUES('Jeans')
+INSERT INTO dbo.Category(CategoryName)VALUES('Pants & Chinos')
+INSERT INTO dbo.Category(CategoryName)VALUES('Shorts')
+INSERT INTO dbo.Category(CategoryName)VALUES('Jean Jackets & Outerwear')
+INSERT INTO dbo.Category(CategoryName)VALUES('Sweatshirts & Sweaters');
+INSERT INTO dbo.Category(CategoryName)VALUES('Shirts & T-Shirts');
+INSERT INTO dbo.Category(CategoryName)VALUES('Accessories');
+
+------ Insert into [SubCategory] ------------
+
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES(N'Straight Jeans',1)
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES(N'Slim Jeans',1)
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES(N'Skinny Jeans',1)
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES(N'Tapered Jeans',1)
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES(N'XX Chino Pants',2)
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES(N'Taper Pants',2)
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES(N'Straight Pants',2)
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES(N'Denim Shorts',3)
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES (N'Cargo Shorts',3);
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES (N'XX Chino Shorts',3);
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES(N'Hoodie Jacket',4)
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES (N'Trucker Jacket',4);
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES (N'Zip-up Hoodie',5);
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES(N'Sweatshirt',5)
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES (N'Hoodie Sweatshirt',5);
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES (N'Crewneck Sweatshirt',5);
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES (N'Western & Denim Shirts',6);
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES (N'Graphic T-Shirts',6);
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES(N'Button-Up Shirts',6)
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES (N'Socks',7);
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES (N'Cap',7);
+INSERT INTO dbo.SubCategory(SubCategoryName,CategoryID)VALUES (N'Bag',7);
+
+------ Insert into ProductStatus ------------
+
+INSERT INTO ProductStatus VALUES ( N'Available');
+INSERT INTO ProductStatus VALUES ( N'StopBusiness');
+
+------ Insert into Brand ------------
+
+INSERT INTO dbo.Brand(BrandName)VALUES(N'Tokyo Life')
+INSERT INTO dbo.Brand(BrandName)VALUES(N'Zombie')
+INSERT INTO dbo.Brand(BrandName)VALUES(N'FREAKERS')
+INSERT INTO dbo.Brand(BrandName)VALUES(N'Karihada')
+INSERT INTO dbo.Brand(BrandName)VALUES(N'Icon Denim')
+INSERT INTO dbo.Brand(BrandName)VALUES(N'Hyperdenim VN')
+INSERT INTO dbo.Brand(BrandName)VALUES(N'Davies')
+INSERT INTO dbo.Brand(BrandName)VALUES(N'EnvyLook')
+INSERT INTO dbo.Brand(BrandName)VALUES(N'Nike')
+INSERT INTO dbo.Brand(BrandName)VALUES(N'Vans')
+INSERT INTO dbo.Brand(BrandName)VALUES(N'Adidas')
+INSERT INTO dbo.Brand(BrandName)VALUES(N'Flaans')
+
+
+--------- Insert into Product -----------
+INSERT INTO Product VALUES (
+	N'ORIGINAL FIT MEN''S JEANS',
+	N'a classic straight leg and iconic styling, they’re literally
+	the blueprint for every pair of jeans in existence — burned into 
+	the world’s collective cortex ever since Levi Strauss (the man himself!) invented them in 1873.',
+	70,
+	65,
+	10,
+	1,
+	3,
+	100,
+	1,
+	11
+);
+
+
+
+--------- Insert into ProductImg ----------
+
+
+--------- Insert into Cart ----------
+
+INSERT INTO Cart VALUES (2, 1, 1);
+
+--------- Insert into [OrderStatus] ----------
+
+INSERT [dbo].[OrderStatus] ([Name]) VALUES (N'Waiting for Confirmation')
+INSERT [dbo].[OrderStatus] ([Name]) VALUES (N'Packaging')
+INSERT [dbo].[OrderStatus] ([Name]) VALUES (N'Delivering')
+INSERT [dbo].[OrderStatus] ([Name]) VALUES (N'Canceled')
+INSERT [dbo].[OrderStatus] ([Name]) VALUES (N'Completed')
+
+--------- Insert into Orders ----------
+
+--------- Insert into [OrderDetail] ------------
+
