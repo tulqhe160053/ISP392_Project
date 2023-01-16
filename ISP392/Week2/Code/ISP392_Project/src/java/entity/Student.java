5bd1185b365d775a92f830fbcde27183f04a9b43
tulqhe160053 <122227097+tulqhe160053@users.noreply.github.com>
2023-01-16 15:55:50 +0700

    Add files via upload

---
+++ ISP392/Week2/Code/ISP392_Project/src/java/entity/Student.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author Tu
+ */
+public class Student {
+    public static void main(String[] args) {
+        
+    }
+}
